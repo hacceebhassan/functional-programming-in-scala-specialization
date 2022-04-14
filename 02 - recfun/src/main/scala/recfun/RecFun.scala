d269d1836322074cc98bf5d42896b77a44736537
hacceebhassan <hacceebhassan@gmail.com>
2022-04-14 16:58:20 +0200

    feat: Week 01 recfun assignment

---
+++ 02 - recfun/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,43 @@
+package recfun
+
+object RecFun extends RecFunInterface:
+
+  def main(args: Array[String]): Unit =
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+      for col <- 0 to row do
+        print(s"${pascal(col, row)} ")
+      println()
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int =
+    if (c==0 || c == r) 1
+    else pascal (c-1, r-1) + pascal (c, r-1)
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean =
+    def balanceWithCounter(chars: List[Char], openParen: Int): Boolean = {
+      if (chars.isEmpty) openParen == 0
+      else {
+        val num =
+          if (chars.head == '(') openParen + 1
+          else if (chars.head == ')') openParen - 1
+          else openParen
+        if (num >= 0) balanceWithCounter(chars.tail, num)
+        else false
+      }
+    }
+    balanceWithCounter(chars, 0)
+
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int =
+    if (money < 0 || coins.isEmpty) 0
+    else if (money == 0) 1
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
